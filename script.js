7084627cb2a0dad203d2387ac8b7f7a610b9e5b0
claireb25 <claire.bourgeois.cb@gmail.com>
2018-03-16 10:34:21 +0100

    commentaire constante

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const carre = document.getElementById('carre');
 // définition de la constante "haut du carré"
 const carreTop = carre.offsetTop;
 //définition de l'événement auquel la modification va se déclencher
+//constante à l'extérieure de la fonction = constante au chargement de la page 
 document.addEventListener('keydown', function mouvement(evenement){
     //console.log("coucou"); //pour tester si l'événement est bien pris en compte
     var stepMove = 10;
